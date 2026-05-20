466fe17e825565149fc924a686336436aec15c8b
Lenovo14\Dario <dsemino@icpayment.net>
2026-05-19 21:02:05 -0300

    Primer prueba de un servidor Express

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("Hola Mundo!!!!");
+});
+
+const PORT = 3000;
+
+app.listen(PORT,() => console.log(`http://localhost:${PORT}`));
